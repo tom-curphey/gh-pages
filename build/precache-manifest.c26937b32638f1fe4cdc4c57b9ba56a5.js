ced118b500a820e3125112bf4617bc76ff7fe994
muppet-training <tomcurphey12@gmail.com>
2019-02-22 14:03:48 +1100

    Build folder

---
+++ build/precache-manifest.c26937b32638f1fe4cdc4c57b9ba56a5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "ca8d0a5d077010f43096",
+    "url": "/static/js/main.ca8d0a5d.chunk.js"
+  },
+  {
+    "revision": "5bf9b2cda7c63b962686",
+    "url": "/static/js/2.5bf9b2cd.chunk.js"
+  },
+  {
+    "revision": "ca8d0a5d077010f43096",
+    "url": "/static/css/main.00c19dc5.chunk.css"
+  },
+  {
+    "revision": "c98c6bb5767576e44b15c2d1d04d4881",
+    "url": "/index.html"
+  }
+];
